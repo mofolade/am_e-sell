aef7bd0026f410b69b9738c2c8eb3331a808671f
Aniko Magyar <mofolade@gmail.com>
2020-11-23 11:06:21 +0100

    Added getAllCategories (controller+service), added findById to the repo.

--- src/main/java/com/example/auctionista/controllers/CategoryController.java
+++ src/main/java/com/example/auctionista/controllers/CategoryController.java
@@ -1,11 +1,22 @@
 package com.example.auctionista.controllers;
 
+import com.example.auctionista.entities.Category;
 import com.example.auctionista.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/rest/categories")
 public class CategoryController {
     @Autowired
     CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
 }
