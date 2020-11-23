ddb175b88f2706290c1d3401226371d3f72ea218
Aniko Magyar <mofolade@gmail.com>
2020-11-23 10:14:27 +0100

    Added search bar.

--- src/main/resources/static/src/views/home.js
+++ src/main/resources/static/src/views/home.js
@@ -6,6 +6,19 @@ export default {
   },
     template: `
       <div class="content">
+        <div id="cover">
+          <form method="get" action="">
+            <div class="tb">
+              <div class="td"><input type="text" placeholder="Search" required></div>
+              <div class="td" id="s-cover">
+                <button type="submit" class="search-button">
+                  <div id="s-circle"></div>
+                  <span></span>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
         <h2>Home Page</h2>
       </div>
     `
