22eb71d2962785462a23127e6f9094746a709d59
Aniko Magyar <mofolade@gmail.com>
2020-11-26 17:04:52 +0100

    Fixed user registration with google account.

--- src/main/java/com/example/auctionista/services/UserService.java
+++ src/main/java/com/example/auctionista/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.auctionista.services;
 
 
+import com.example.auctionista.entities.User;
 import com.example.auctionista.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,6 +11,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    public User findCurrentUser() {
+        // the login session is stored between page reloads,
+        // and we can access the current authenticated user with this
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepo.findByEmail(email);
+    }
+
+    public User registerUser(String name, String email, String password, String picture_url, String image_url, String organize_number) {
+        return myUserDetailsService.addUser(name, email, password, picture_url, image_url, organize_number);
+    }
 
 
 }
