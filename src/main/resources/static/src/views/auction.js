d6e54e77610a91f81a1432ac5dd3f88e709208f5
Aniko Magyar <mofolade@gmail.com>
2020-11-28 22:51:32 +0100

    Added countdown function.

--- src/main/resources/static/src/views/auction.js
+++ src/main/resources/static/src/views/auction.js
@@ -66,6 +66,7 @@ export default {
                     <section>
                       <label>Slutar:</label>
                       {{stopDateTime}}
+                      <div id="countdown"></div>
                     </section>
                   </div>
                   <section class="bid-section" v-bind:class="visibleCheck ? 'isVisble' : 'notVisible'">
@@ -129,6 +130,9 @@ export default {
         visibleCheck: false
     }
   },
+  created() {
+    this.countDown();
+  },
   computed: {
     user() {
         return this.$store.state.user
@@ -177,6 +181,36 @@ export default {
     },    
     closeForm() {
       document.getElementById("myChatBoxForm").style.display = "none";
+    },
+    countDown(stop_date){
+      console.log(stop_date)
+      var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+      // Update the count down every 1 second
+      var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+          
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+          
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+          
+        // Output the result in an element with id="demo"
+        document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+          
+        // If the count down is over, write some text 
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+      }, 1000);
     }
   },
   }
